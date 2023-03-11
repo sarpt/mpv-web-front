ad3a2c39490f299a29b17463a1f93f5f1c6d0f6f
sarpt <mike.m.info@gmail.com>
2023-03-11 10:55:40 +0100

    Add AudioSubtitle dialog

--- src/delivery/ui/pages/common/PlaybackSummary.tsx
+++ src/delivery/ui/pages/common/PlaybackSummary.tsx
@@ -1,16 +1,18 @@
 import { IconButton, styled } from "@mui/material";
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import LanguageIcon from '@mui/icons-material/Language';
 import { Fullscreen, FullscreenExit, Loop, Pause } from "@mui/icons-material";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
 
 import { selectMediaFiles } from "../../plocs/media_files/selectors";
-import { fullscreen, loop, pause, subscribeToPlayback, unsubscribeToPlayback } from "../../plocs/playback/actions";
+import { changeAudio, changeSubtitles, fullscreen, loop, pause, subscribeToPlayback, unsubscribeToPlayback } from "../../plocs/playback/actions";
 import { selectPlayback } from "../../plocs/playback/selectors";
 import { secondsToHHMMSS } from "../../plocs/playback/functions/formatTime";
 import { LoopDialog } from "./LoopDialog";
 import { LoopVariant } from "../../plocs/playback/models";
+import { AudioSubtitleDialog } from "./AudioSubtitleDialog";
 
 const PlaybackProgress = styled(LinearProgress)(({
   flexGrow: 1,
@@ -58,6 +60,7 @@ const PlaybackControlButton = styled(IconButton)(({ theme }) => {
 
 export const PlaybackSummary = () => {
   const [loopDialogOpen, setLoopDialogOpen] = useState<boolean>(false);
+  const [languageDialogOpen, setLanguageDialogOpen] = useState<boolean>(false);
 
   const playback = useSelector(selectPlayback);
   const mediaFiles = useSelector(selectMediaFiles);
@@ -103,6 +106,12 @@ export const PlaybackSummary = () => {
   const toggleFullscreen = useCallback(() => {
     dispatch(fullscreen(!playback?.Fullscreen));
   }, [playback?.Fullscreen]);
+
+  const onLanguageChange = useCallback((args: { subtitleId?: string, audioId?: string }) => {
+    if (args.subtitleId && args.subtitleId !== playback?.SelectedSubtitleID) dispatch(changeSubtitles(args.subtitleId));
+    if (args.audioId && args.audioId !== playback?.SelectedAudioID) dispatch(changeAudio(args.audioId));
+    setLoopDialogOpen(false); 
+  }, [playback?.SelectedSubtitleID, playback?.SelectedAudioID]);
 
   const onLoopChange = useCallback((variant: LoopVariant) => {
     dispatch(loop(variant));
@@ -154,11 +163,24 @@ export const PlaybackSummary = () => {
         >
           <Loop />
         </PlaybackControlButton>
+        <PlaybackControlButton
+          aria-label="language"
+          onClick={() => setLanguageDialogOpen(true)}
+          disabled={!mediaFileSelected}
+        >
+          <LanguageIcon />
+        </PlaybackControlButton>
         <LoopDialog
           currentVariant={playback?.Loop.Variant ?? LoopVariant.Off}
           open={loopDialogOpen}
           onOk={onLoopChange}
           onClose={() => setLoopDialogOpen(false)}
+        />
+        <AudioSubtitleDialog
+          mediaFile={currentMediaFile}
+          open={languageDialogOpen}
+          onOk={onLanguageChange}
+          onClose={() => setLanguageDialogOpen(false)}
         />
       </ButtonsContainer>
     </PlaybackContainer>
