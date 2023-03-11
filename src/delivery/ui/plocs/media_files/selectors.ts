2fe18e35acbf8e8fa9bb512b4427ca4a16b4a49c
sarpt <mike.m.info@gmail.com>
2023-03-11 15:30:43 +0100

    Improve redraws performance: create media files list custom selector + create dedicated playback selectors

--- src/delivery/ui/plocs/media_files/selectors.ts
+++ src/delivery/ui/plocs/media_files/selectors.ts
@@ -1,9 +1,24 @@
+import { createSelectorCreator, defaultMemoize } from 'reselect';
 import { createSelector } from "@reduxjs/toolkit"
 import { selectStoreState } from "../../reducers";
+import { MediaFilesMap } from './models';
 
 const selectMediaFilesState = createSelector(selectStoreState, (state) => state.mediaFiles);
 
-export const selectMediaFiles = createSelector(
+const getMediaFilesListIdentity = (mediaFiles: MediaFilesMap) => Object.keys(mediaFiles).join(';');
+const createMediaFilesSelector = createSelectorCreator(
+  defaultMemoize,
+  (a: MediaFilesMap, b: MediaFilesMap): boolean => {
+    return getMediaFilesListIdentity(a) === getMediaFilesListIdentity(b)
+  }
+)
+
+const selectMediaFilesList = createSelector(
   selectMediaFilesState,
   (state) => state.mediaFiles
+);
+
+export const selectMediaFiles = createMediaFilesSelector(
+  selectMediaFilesList,
+  (mediaFilesMap) => mediaFilesMap,
 );
