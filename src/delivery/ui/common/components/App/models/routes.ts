412dfb14fb0fa2a589aa1f3d4a2afa256c1bbe4a
sarpt <mike.m.info@gmail.com>
2023-10-07 11:52:04 +0200

    Fix routing

---
+++ src/delivery/ui/common/components/App/models/routes.ts
@@ -0,0 +1,4 @@
+export enum Routes {
+  MediaFiles = 'mediafiles',
+  Playlists = 'playlists'
+}
--- src/routing.tsx
+++ src/routing.tsx
@@ -4,16 +4,28 @@ import {
 } from "react-router-dom";
 import { MediaFilesPage } from "./delivery/ui/pages/media_files/MediaFilesPage";
 import { PlaylistsPage } from "./delivery/ui/pages/playlists/PlaylistsPage";
+import { AppLayout } from "./delivery/ui/common/layouts/AppLayout";
+import { Routes } from "./delivery/ui/common/components/App/models/routes";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <MediaFilesPage />,
-  },
-  {
-    path: "/playlists",
-    element: <PlaylistsPage />,
-  },
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      {
+        path: Routes.MediaFiles,
+        element: <MediaFilesPage />,
+      },
+      {
+        path: Routes.Playlists,
+        element: <PlaylistsPage />,
+      },
+      {
+        path: '/',
+        element: <MediaFilesPage />,
+      }
+    ]
+  }
 ]);
 
 export const Routing = () => {
