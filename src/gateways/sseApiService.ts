69805a7dc589370643cf7921859a0ffc6fc00c93
sarpt <mike.m.info@gmail.com>
2025-05-09 18:36:28 +0200

    Remove redundant media files events enum

--- src/gateways/sseApiService.ts
+++ src/gateways/sseApiService.ts
@@ -2,18 +2,13 @@ import { makeErr, makeOk, Result } from "src/domains/common/either";
 import { ApiService } from "src/domains/connection/interfaces";
 import { MediaFilesMap } from "src/domains/media_files/entities";
 import { MediaFilesEvents } from "src/domains/media_files/interfaces";
+import { PlaybackEvents } from "src/domains/playback/interfaces";
 import { EventsObserver } from "src/gateways/eventsObserver";
 
 export enum Domains {
   MediaFiles = 'mediaFiles',
   Playback = 'playback',
   Playlists = 'playlists'
-}
-
-export enum MediaFilesChannelEvents {
-  Added = 'added',
-  Removed = 'removed',
-  Replay = 'replay'
 }
 
 const sseChannels = Object.values(Domains);
@@ -51,21 +46,21 @@ export class SSEApiService implements ApiService {
     // start iterating over event observer at the init
     // without this the first call to "next" on generator from event observer might be too late and events can be missed
     this.mediaFilesIterator = this.eventObserver.aggregate({
-        [createChannelEvent(Domains.MediaFiles, MediaFilesChannelEvents.Added)]: (payload) => {
+        [createChannelEvent(Domains.MediaFiles, MediaFilesEvents.Added)]: (payload) => {
           return {
             eventVariant: MediaFilesEvents.Added,
             payload: JSON.parse(payload) as MediaFilesMap
           };
         },
-        [createChannelEvent(Domains.MediaFiles, MediaFilesChannelEvents.Removed)]: (payload) => {
+        [createChannelEvent(Domains.MediaFiles, MediaFilesEvents.Removed)]: (payload) => {
           return {
             eventVariant: MediaFilesEvents.Removed,
             payload: JSON.parse(payload) as MediaFilesMap
           };
         },
-        [createChannelEvent(Domains.MediaFiles, MediaFilesChannelEvents.Replay)]: (payload) => {
+        [createChannelEvent(Domains.MediaFiles, MediaFilesEvents.Replay)]: (payload) => {
           return {
-            eventVariant: MediaFilesEvents.Added,
+            eventVariant: MediaFilesEvents.Replay,
             payload: JSON.parse(payload) as MediaFilesMap
           };
         },
@@ -73,12 +68,13 @@ export class SSEApiService implements ApiService {
   }
 
   iterateMediaFiles() {
-    if (!this.mediaFilesIterator) return makeErr(new Error('connection to server SSE has not been estabilished'));
+    const iterator = this.mediaFilesIterator;
+    if (!iterator) return makeErr(new Error('connection to server SSE has not been estabilished'));
 
-    return makeOk(this.mediaFilesIterator);
+    return makeOk(iterator);
   }
 }
 
-export function createChannelEvent(domain: Domains, ev: MediaFilesChannelEvents): string {
+export function createChannelEvent(domain: Domains, ev: MediaFilesEvents | PlaybackEvents): string {
   return `${domain}.${ev}`;
 }
