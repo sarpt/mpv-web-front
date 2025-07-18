d6e6cd55ba782b600a86ef23b1bee12665138628
sarpt <mike.m.info@gmail.com>
2025-07-18 22:28:07 +0200

    Show update progress & info about success and reload needed

--- src/delivery/ui/pages/components/AppVersion.tsx
+++ src/delivery/ui/pages/components/AppVersion.tsx
@@ -1,23 +1,32 @@
-import { Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, Link, styled } from "@mui/material";
+import { Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, LinearProgress, Link, styled } from "@mui/material";
 import { useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { FrontendPackageRelease } from "src/domains/packages/entities";
 import { updateFrontend } from "ui/plocs/packages/actions";
-import { selectCheckingLatestFrontendRelease, selectLatestFrontendRelease, selectShouldUpdateFrontend } from "ui/plocs/packages/selectors";
+import {
+  selectCheckingLatestFrontendRelease,
+  selectFrontendReloadNeeded,
+  selectIsFrontendUpdateInProgress,
+  selectLatestFrontendRelease,
+  selectShouldUpdateFrontend
+} from "ui/plocs/packages/selectors";
 
 export const AppVersion = () => {
   const dispatch = useDispatch();
   const [updateDialogOpen, setUpdateDialogOpen] = useState<boolean>(false);
 
   const latestVersionCheckInProgress = useSelector(selectCheckingLatestFrontendRelease);
+  const isFrontendUpdateInProgress = useSelector(selectIsFrontendUpdateInProgress);
   const latestVersionInfo = useSelector(selectLatestFrontendRelease);
   const shouldUpdateFrontend = useSelector(selectShouldUpdateFrontend);
+  const frontendReloadNeeded = useSelector(selectFrontendReloadNeeded);
 
   const updateRelease = useCallback(() => {
     if (!latestVersionInfo) return;
 
     dispatch(updateFrontend(latestVersionInfo));
   }, [dispatch, latestVersionInfo]);
+
 
   return (
     <VersionInfoContainer>
@@ -29,10 +38,13 @@ export const AppVersion = () => {
         onUpdate={() => setUpdateDialogOpen(true)}
       />
       <UpdateDialog
+        isFrontendUpdateInProgress={isFrontendUpdateInProgress}
+        frontendReloadNeeded={frontendReloadNeeded}
         open={updateDialogOpen}
         pkgRelease={latestVersionInfo}
         onClose={() => setUpdateDialogOpen(false)}
         onUpdate={updateRelease}
+        onReload={onReload}
       />
     </VersionInfoContainer>
   );
@@ -66,24 +78,24 @@ const LatestVersion = ({
   )
 };
 
-const VersionInfoContainer = styled('div')`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
 type UpdateDialogProps = {
+  isFrontendUpdateInProgress: boolean,
+  frontendReloadNeeded: boolean,
   open: boolean,
   onUpdate: () => void,
   onClose: () => void,
   pkgRelease: FrontendPackageRelease | undefined,
+  onReload: () => void,
 };
 
 const UpdateDialog = ({
+  isFrontendUpdateInProgress,
+  frontendReloadNeeded,
   open,
   pkgRelease,
   onUpdate,
-  onClose
+  onClose,
+  onReload
 }: UpdateDialogProps) => {
   if (!pkgRelease) return <></>;
 
@@ -91,7 +103,27 @@ const UpdateDialog = ({
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Frontend version {pkgRelease.version}</DialogTitle>
       <DialogContent>
-        {pkgRelease.description}
+        <UpdateDialogContent>
+          <span>{pkgRelease.description}</span>
+          {
+            isFrontendUpdateInProgress && (
+              <UpdateProgressContainer>
+                <LinearProgress variant="indeterminate" />
+                <span>Update in progress...</span>
+              </UpdateProgressContainer>
+            )
+          }
+          {
+            frontendReloadNeeded && (
+              <UpdateSuccessContainer>
+                <UpdateSuccededInfo>Update succeeded</UpdateSuccededInfo>
+                <Link component="button" onClick={onReload}>
+                  Please reload the browser tab.
+                </Link>
+              </UpdateSuccessContainer>
+            )
+          }
+        </UpdateDialogContent>
       </DialogContent>
       <DialogActions>
         <Button
@@ -103,10 +135,41 @@ const UpdateDialog = ({
           variant="contained"
           color="primary"
           onClick={onUpdate}
+          disabled={isFrontendUpdateInProgress || frontendReloadNeeded}
         >
           Update
         </Button>
       </DialogActions>
     </Dialog>
   );
+};
+
+const ColumnFlex = styled('div')`
+  display: flex;
+  flex-direction: column;
+`;
+
+const UpdateDialogContent = styled(ColumnFlex)`
+  gap: 8px;
+`;
+
+const UpdateSuccededInfo = styled('span')`
+  color: green;  
+  font-weight: bold;
+`;
+
+const UpdateProgressContainer = styled(ColumnFlex)`
+  text-align: center;
+`;
+
+const UpdateSuccessContainer = styled(ColumnFlex)`
+  align-items: center;
+`;
+
+const VersionInfoContainer = styled(ColumnFlex)`
+  align-items: center;
+`;
+
+const onReload = () => {
+  window.location.reload();
 };
